0692c4ee2a2cc65ea106a6457255c054388fb362
Mike Verderese <mike@useodin.com>
2024-04-05 12:39:54 -0400

    Add prettier to project

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next/core-web-vitals", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+};
