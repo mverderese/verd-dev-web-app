1d08bf132bf22e08f49fc5569d95dbc84a3561da
Mike Verderese <mike@useodin.com>
2024-04-05 15:24:31 -0400

    Add GTM code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const isProdNodeEnv = process.env.NODE_ENV === "production";
+const gtmMeasurementId = process.env.GTM_MEASUREMENT_ID;
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +20,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {isProdNodeEnv && gtmMeasurementId && (
+        <Script id="gtm">
+          {`
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=${gtmMeasurementId}"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', ${gtmMeasurementId});
+            </script>
+          `}
+        </Script>
+      )}
       <body className={inter.className}>{children}</body>
     </html>
   );
