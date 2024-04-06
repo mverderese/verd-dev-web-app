d220b4de209657fa4f5692eea7ec9d1fd2deceda
Mike Verderese <mike@useodin.com>
2024-04-06 10:24:06 -0400

    Add IAM to terraform

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -13,8 +13,39 @@ provider "google" {
   region      = var.region
   zone        = var.zone
   default_labels = {
-    management = "terraform"
+    management  = "terraform"
+    environment = var.environment
   }
+}
+
+resource "google_project" "verderese_development_project" {
+  name            = "Verderese Development"
+  project_id      = "verderese-development"
+  billing_account = "01F4FF-081A6A-AE3441"
+}
+
+resource "google_service_account" "verd_dev_web_app_sa" {
+  account_id   = "verd-dev-web-app-${var.environment}"
+  display_name = "Verd Dev Web App ${title(var.environment)} service account"
+}
+
+resource "google_project_iam_binding" "verd_dev_web_app_sa_iam_binding" {
+  for_each = toset(["roles/cloudsql.client", "roles/secretmanager.secretAccessor"])
+  project  = var.project
+  role     = each.key
+  members  = ["serviceAccount:${google_service_account.verd_dev_web_app_sa.email}"]
+}
+
+resource "google_service_account" "github_actions_sa" {
+  account_id   = "github-actions-${var.environment}"
+  display_name = "GitHub Actions ${title(var.environment)} service account"
+}
+
+resource "google_project_iam_binding" "github_actions_sa_iam_binding" {
+  for_each = toset(["roles/artifactregistry.writer", "roles/run.admin", "roles/secretmanager.viewer", "roles/secretmanager.secretAccessor"])
+  project  = var.project
+  role     = each.key
+  members  = ["serviceAccount:${google_service_account.github_actions_sa.email}"]
 }
 
 resource "google_compute_network" "vpc_network" {
@@ -44,3 +75,4 @@ resource "google_dns_managed_zone" "dns_managed_zone" {
     }
   }
 }
+
