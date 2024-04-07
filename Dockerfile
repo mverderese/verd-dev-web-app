8ac7d9d83c6e85379ead04674d4650a695c4c558
Mike Verderese <mike@useodin.com>
2024-04-06 23:13:02 -0400

    Pass environment variables and build time

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:20.12.1-alpine
 COPY . /app
 WORKDIR /app
 
+RUN cat /app/.env
+
 RUN npm install -g npm
 RUN npm ci
 RUN npm run build
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ${gtmMeasurementId});
+              gtag('config', '${gtmMeasurementId}');
             </script>
           `}
         </Script>
