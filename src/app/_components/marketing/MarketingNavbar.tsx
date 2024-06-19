361fcbfbe2fedf65eac3c3be28f099139866c2b3
Mike Verderese <mike@useodin.com>
2024-06-19 16:27:49 -0400

    Strip site down to most basic form for now

--- src/app/_components/marketing/MarketingNavbar.tsx
+++ src/app/_components/marketing/MarketingNavbar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { APP_TITLE, MY_NAME } from "@/app/_components/shared/constants";
-import MenuItem from "@/app/_components/marketing/MenuItem";
+import { MY_NAME } from "@/app/_components/shared/constants";
 import GithubButton from "@/app/_components/shared/GithubButton";
 
 export default function MarketingNavbar() {
@@ -61,9 +60,9 @@ export default function MarketingNavbar() {
           className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
         >
           <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7">
-            <MenuItem title={"About"} link="/about" />
-            <MenuItem title={"Portfolio"} link="/portfolio" />
-            <MenuItem title={APP_TITLE} link="/app" />
+            {/*<MenuItem title={"About"} link="/about" />*/}
+            {/*<MenuItem title={"Portfolio"} link="/portfolio" />*/}
+            {/*<MenuItem title={APP_TITLE} link="/app" />*/}
             <GithubButton />
           </div>
         </div>
