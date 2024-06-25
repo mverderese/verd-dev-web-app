cf11ac61ee840a5e8e93ef4ac687757d6cc7468d
Mike Verderese <mike@useodin.com>
2024-06-24 21:08:29 -0400

    Add bolder design

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -4,6 +4,9 @@ import "../globals.css";
 import PrelineScript from "@/app/_components/shared/PrelineScript";
 import MarketingNavbar from "@/app/_components/marketing/MarketingNavbar";
 import { IS_INDEXABLE, JOB_TITLE, MY_NAME, SITE_DESCRIPTION } from "@/app/constants";
+import localFont from "next/font/local";
+import { Archivo_Black } from "next/font/google";
+import classNames from "classnames";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 const appEnv = process.env.NEXT_PUBLIC_APP_ENV;
@@ -30,14 +33,22 @@ export const metadata: Metadata = {
   },
 };
 
+const akiraExpandedFont = localFont({
+  src: "../_fonts/akira-expanded.otf",
+  display: "swap",
+  variable: "--font-akira-expanded",
+});
+
+const archivoBlackFont = Archivo_Black({ weight: "400", variable: "--font-archivo-black", subsets: ["latin"] });
+
 export default function MarketingLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-gray-100">
+    <html lang="en" className={classNames(akiraExpandedFont.variable, archivoBlackFont.variable)}>
+      <body className={classNames("bg-gray-100")}>
         <MarketingNavbar />
         <main>{children}</main>
       </body>
