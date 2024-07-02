aa13731650619567ce5e9e5c48fb1d612c5a0896
Mike Verderese <mike@useodin.com>
2024-07-02 12:16:13 -0400

    Add titles to all pages

--- src/app/(marketing)/portfolio/projects/[slug]/page.tsx
+++ src/app/(marketing)/portfolio/projects/[slug]/page.tsx
@@ -1,10 +1,21 @@
 import { portfolioProjects } from "@/app/(marketing)/portfolio/_data";
 import H1 from "@/app/(marketing)/_components/H1";
 import ProjectHeroPanel from "@/app/(marketing)/portfolio/projects/_components/ProjectHeroPanel";
+import { Metadata } from "next";
+import { titleForEnv } from "@/app/_util";
+import { MY_NAME } from "@/app/constants";
 
 export type ProjectPageProps = {
   params: { slug: string };
 };
+
+export function generateMetadata({ params: { slug } }: ProjectPageProps): Metadata {
+  const project = portfolioProjects.find((p) => p.slug === slug);
+
+  return {
+    title: titleForEnv(`${project?.title ?? "Unknown Project"} | ${MY_NAME}`),
+  };
+}
 
 export default function ProjectPage({ params: { slug } }: ProjectPageProps) {
   const project = portfolioProjects.find((p) => p.slug === slug);
